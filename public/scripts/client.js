ddf840bffd605f87a43e2b71e88695f1fa2a52c2
Cameron <camglowicki@hotmail.com>
2020-12-16 21:31:10 +0000

    implemented createTweetElement and renderTweet functions to produce produce tweets from mock database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1607976643950
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1608063043950
+  }
+]
+
+$(document).ready(function() {
+
+const createTweetElement = function (data) {
+  // $tweet, template from index.html
+  const $tweet = 
+  $(`<article class="tweet">
+  <header>
+  <p id="header-name">${data.user["name"]}</p>
+  <p id="header-handle">${data.user["handle"]}</p>
+  </header>
+  <p id="tweet-body">${data.content["text"]}</p>
+  <footer>${data["created_at"]}</footer>
+  </article>`);
+  return $tweet;
+};
+
+const renderTweets = function (data) {
+  for (let info of data) {
+    //loop and createTweetElement for each item in data
+    const $tweet = createTweetElement(info);
+    $('#tweet-container').append($tweet);
+  }
+};
+
+renderTweets(data);
+});
+
+
