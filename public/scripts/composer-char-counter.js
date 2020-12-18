f2411acca1d4087d38418fb0025829e2b1589b19
Cameron <camglowicki@hotmail.com>
2020-12-18 00:03:42 +0000

    added dynamic error message for tweets > 140 chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('.new-tweet').on('keyup', 'textarea', function(evt) { 
+  $('.new-tweet').on('keyup', 'textarea', function (evt) {
+
     //.new-tweet is parent to textarea and counter
     //action happens on textarea
-    const counter =  $(this).parent().find(".counter")
+    const counter = $(this).parent().find(".counter")
     //find child .counter of .new-tweet
     let length = 140 - this.textLength;
-    //console.log(this);
     $(counter).text(length);
-    
+
+    // dynamic error message for length > 140
+    if (this.textLength > 140) {
+      $('#error').html('Your song is too long...');
+    } else {
+      $('#error').html(null);
+    }
   })
 
 });
